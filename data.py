4d162588548d2a4d82b470c297722e2b754aa16b
Ram <rmalav15@gmail.com>
2019-03-21 01:39:35 +0530

    added red boxes for truncated and difficult bbox

--- data.py
+++ data.py
@@ -1,16 +1,21 @@
 import os
 from bs4 import BeautifulSoup
 
+
 class Entity():
-    def __init__(self, name, xmin, xmax, ymin, ymax):
+    def __init__(self, name, xmin, xmax, ymin, ymax, difficult, truncated):
         self.name = name
         self.xmin = xmin
         self.xmax = xmax
         self.ymin = ymin
         self.ymax = ymax
+        self.difficult = difficult
+        self.truncated = truncated
+
 
 class Data():
-    def  __init__(self, root_dir, image_name):
+    def __init__(self, root_dir, image_name):
+        self.image_name = image_name
         self.image_path = os.path.join(root_dir, "JPEGImages", image_name + ".jpg")
         self.annotation_path = os.path.join(root_dir, "Annotations", image_name + ".xml")
         self.annotations = self.load_masks()
@@ -22,10 +27,12 @@ class Data():
         objs = bs.findAll('object')
         for obj in objs:
             obj_name = obj.findChildren('name')[0].text
+            difficult = int(obj.findChildren('difficult')[0].contents[0])
+            truncated = int(obj.findChildren('truncated')[0].contents[0])
             bbox = obj.findChildren('bndbox')[0]
             xmin = int(bbox.findChildren('xmin')[0].contents[0])
             ymin = int(bbox.findChildren('ymin')[0].contents[0])
             xmax = int(bbox.findChildren('xmax')[0].contents[0])
             ymax = int(bbox.findChildren('ymax')[0].contents[0])
-            annotations.append(Entity(obj_name, xmin, xmax, ymin, ymax))
+            annotations.append(Entity(obj_name, xmin, xmax, ymin, ymax, difficult, truncated))
         return annotations
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import cv2
 parser = argparse.ArgumentParser()
 parser.add_argument("--root_dir", type=str, default="/mnt/069A453E9A452B8D/Ram/surveillance-data/sdd_train")
 parser.add_argument("--type", type=str, default="train", help="train|val|trainval|test")
-parser.add_argument("--random_seed", type=int, default=1000)
-parser.add_argument("--label_set", type=str, default="")
+parser.add_argument("--random_seed", type=int, default=100)
+parser.add_argument("--save_images", type=bool, default=True)
+parser.add_argument("--save_dir", type=str, default="output")
 parser.add_argument("--line_thickness", type=int, default=5)
 args = parser.parse_args()
 random.seed(args.random_seed)
@@ -25,9 +26,13 @@ def get_image_list(dir, filename):
 
 def process_image(image_data):
     image = cv2.imread(image_data.image_path)
+    image = cv2.putText(image, image_data.image_name, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
     for ann in image_data.annotations:
-        image = cv2.rectangle(image, (ann.xmin, ann.ymin), (ann.xmax, ann.ymax), (0, 255, 0), args.line_thickness)
-        image = cv2.putText(image, ann.name, (ann.xmin, ann.ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        box_color = (0, 255, 0)  #Green
+        if ann.difficult or ann.truncated:
+            box_color = (0, 0, 255) #Red
+        image = cv2.rectangle(image, (ann.xmin, ann.ymin), (ann.xmax, ann.ymax), box_color, args.line_thickness)
+        image = cv2.putText(image, ann.name, (ann.xmin, ann.ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
     return image
 
 
@@ -39,6 +44,8 @@ def main(args):
     while True:
         image_data = Data(args.root_dir, image_list[index])
         image = process_image(image_data)
+        if args.save_images:
+            cv2.imwrite(os.path.join(args.save_dir, image_list[index] + ".jpg"), image)
         cv2.imshow('image', image)
         k = chr(cv2.waitKey())
         if k == 'd':  # next
@@ -47,9 +54,9 @@ def main(args):
             index = index - 1 if index != 0 else total_images - 1
         elif k == 's':
             index = random.randint(0, total_images)
-        elif k == 27:
-            cv2.waitKey(2)
+        elif k == 'q':
             cv2.destroyAllWindows()
+            cv2.waitKey(1)
             break
 
 
