6e5c93f65919d1353ebc2cf912d8f425c311b2b7
zhangwenwen <zhangwenwen1000@gmail.com>
2020-12-16 21:47:51 +0800

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ parser.add_argument("--type", type=str, default="train", help="train|val|trainva
 parser.add_argument("--random_seed", type=int, default=100)
 parser.add_argument("--save_images", type=bool, default=True)
 parser.add_argument("--save_dir", type=str, default="output")
-parser.add_argument("--line_thickness", type=int, default=5)
+parser.add_argument("--line_thickness", type=int, default=1)
 parser.add_argument("--with_mask",action="store_true")
 parser.add_argument("--with_bbox",action="store_true")
 args = parser.parse_args()
@@ -31,14 +31,14 @@ def process_image(image_data,with_mask=False,with_bbox=False):
     if not os.path.exists(image_data.image_path):
         image_data.image_path=image_data.image_path.replace('jpg','png')
     image = cv2.imread(image_data.image_path)
-    image = cv2.putText(image, image_data.image_name, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    image = cv2.putText(image, image_data.image_name, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2)
     if with_bbox:
         for ann in image_data.annotations:
             box_color = (0, 255, 0)  #Green
             if ann.difficult or ann.truncated:
                 box_color = (0, 0, 255) #Red
             image = cv2.rectangle(image, (ann.xmin, ann.ymin), (ann.xmax, ann.ymax), box_color, args.line_thickness)
-            image = cv2.putText(image, ann.name, (ann.xmin, ann.ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+            image = cv2.putText(image, ann.name, (ann.xmin, ann.ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
     if with_mask:
         if os.path.exists(image_data.mask_path):
             mask_img=cv2.imread(image_data.mask_path)
